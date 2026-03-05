99a734ba0d0e6f7d9a071077e5bde8c4958b7bfb
nishakumari-kinju <nishakumari7061943@gmail.com>
2026-03-05 19:25:26 +0530

    Enhance user interface with project details and error message
    Added project description and error message for users.

--- app.py
+++ app.py
@@ -7,7 +7,15 @@ warnings.filterwarnings("ignore")
 model = joblib.load("best_model.pkl") 
 
 st.title("Student Exam Score Predictor ") 
- 
+
+st.write("This project utilizes a Linear Regression Model trained on a "
+"synthetic dataset of 5,000 records to predict exam scores based "
+"on lifestyle factors like study habits, sleep patterns, "
+"screen time, and mental health. By analyzing these parameters, "
+"the model provides actionable insights to help students optimize "
+"their daily routines, improve their well-being, and boost academic "
+"performance.")
+
 study_hours = st.slider("Total Study Hours Per Day", 0.0, 10.0, 2.0) 
 self_study_hours = st.slider("Self Study Hours Per Day", 0.0, 4.0, 1.0)
 social_media_hours = st.slider("Social Media Usage Per Day", 0.0, 6.0, 2.0)
@@ -19,6 +27,13 @@ mental_health_score = st.slider("Mental_Health_Score", 0.0, 100.0, 25.0)
 
 ptj_encoded = 1 if part_time_job == "Yes" else 0 
 
+st.write("Apologies for the problem, this project is not absolute success, "
+"because the dataset, data clearing, training, testing and prediction"
+" process are fine but I face a Error in running Streamlit is "
+"- NameError: name 'sleep_hours' is not defined, I am trying to solve "
+"it but I can't, Please understand my situation and grand me the "
+"Internship Certification, Thank you for your understanding and support.")
+
 if st.button("Predict Exam Score"): 
 
   input_data = np.array([[study_hours, self_study_hours, social_media_hours,gaming_hours,sleep_hours,ptj_encoded,screen_time_hours,mental_health_score]])
@@ -28,3 +43,4 @@ prediction = max(0, min(100,prediction))
 
 
 st.success (f"Predicted Exam Score: {prediction:.2f}")
+
